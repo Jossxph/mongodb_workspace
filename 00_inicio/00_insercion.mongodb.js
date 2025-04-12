ea73bf0f8b0d3cdcd99b841119e937954ab2c80a
Jossxph <jossxph@gmail.com>
2025-04-12 02:08:34 +0000

    Creacion de una BD, Coleccion

---
+++ 00_inicio/00_insercion.mongodb.js
@@ -0,0 +1,22 @@
+// VAMOS A CREA BD
+
+use("sales_clothes")
+
+
+/* db.prendas.insertOne({
+    nombre: "Camisa",
+    talla: "M",
+    color: "Blanco",
+    precio: 29.99,
+    stock: 50,
+    categoria: "Ropa de hombre",
+    marca: "EjemploBrand",
+});
+ */
+
+
+db.prendas.find()
+
+
+
+
